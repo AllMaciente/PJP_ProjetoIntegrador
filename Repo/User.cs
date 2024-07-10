66e34c863c78132f7acb95ff1db9845bfd87f62e
Allan <135379484+AllMaciente@users.noreply.github.com>
2024-07-10 19:53:02 -0300

    Update User.cs

--- Repo/User.cs
+++ Repo/User.cs
@@ -56,5 +56,87 @@ namespace Repo
             CloseConexao();
             return users;
         }
+
+         public static void Criar(User user) {
+            InitConexao();
+            string insert = "INSERT INTO usuarios (nome, data_nascimento, cpf) VALUES (@Nome, @Data_nascimento, @Cpf)";
+            MySqlCommand command = new MySqlCommand(insert, conexao);
+            try {
+                if(user.Nome == null || user.Data_nascimento == null || user.Cpf == null) {
+                    MessageBox.Show("Deu ruim, favor preencher a pessoa");
+                } else {
+                    command.Parameters.AddWithValue("@Nome", user.Usuario);
+                    command.Parameters.AddWithValue("@Data_nascimento", user.Data_nascimento);
+                    command.Parameters.AddWithValue("@Cpf", user.Cpf);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+                    user.Id = Convert.ToInt32(command.LastInsertedId);
+
+                    if(rowsAffected > 0){
+                        MessageBox.Show("Usuario cadastrada com sucesso");
+                        users.Add(pessoa);
+                    } else {
+                        MessageBox.Show("Deu ruim, não deu pra adicionar");
+                    }
+                }
+            } catch (Exception e) {
+                MessageBox.Show("Deu ruim: " + e.Message);
+            }
+
+            CloseConexao();
+        }
+
+        public static void UpdatePessoa(
+            int indice,
+            string usuario,
+            int data_nascimento,
+            string cpf
+        ){
+            InitConexao();
+            MessageBox.Show("iniciando");
+            string query = "UPDATE usuarios SET nome = @Nome, data_nascimento = @Data_nascimento, cpf = @Cpf WHERE id_usuario = @Id";
+            MySqlCommand command = new MySqlCommand(query, conexao);
+            User user = users[indice];
+            try {
+                if(nome != null || idade > 0 || cpf != null) {
+                    command.Parameters.AddWithValue("@Id", pessoa.Id);
+                    command.Parameters.AddWithValue("@Nome", usuario);
+                    command.Parameters.AddWithValue("@Cpf", cpf);
+                    command.Parameters.AddWithValue("@Data_nascimento", data_nascimento);
+                    int rowsAffected = command.ExecuteNonQuery();
+                
+                    if (rowsAffected > 0) {
+                        user.Usuario = nome;
+                        user.Data_nascimento = data_nascimento;
+                        user.Cpf = cpf;
+                    }
+                    else {
+                        MessageBox.Show(rowsAffected.ToString());
+                    }
+                }else {
+                    MessageBox.Show("Usuário não encontrado");
+                }
+            }catch (Exception ex){
+                MessageBox.Show("Erro durante a execução do comando: " + ex.Message);
+            }
+            CloseConexao();
+        }
+
+        public static void Delete(int index) {
+            InitConexao();
+            string delete = "DELETE FROM usuarios WHERE id_usuario = @Id";
+            MySqlCommand command = new MySqlCommand(delete, conexao);
+            command.Parameters.AddWithValue("@Id", users[index].Id);
+            // executar
+            int rowsAffected = command.ExecuteNonQuery();
+            if(rowsAffected > 0) {
+                pessoas.RemoveAt(index);
+                MessageBox.Show("usuário deletada com sucesso.");
+            } else {
+                MessageBox.Show("Usuário não encontrado.");
+            }
+            CloseConexao();
+        }
+
     }
 }
