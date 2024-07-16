f7cf90ca520ce96636bad5e0a1a55c5e5dd8388d
Allan <135379484+AllMaciente@users.noreply.github.com>
2024-07-15 21:16:16 -0300

    update sql

--- BD/database.sql
+++ BD/database.sql
@@ -1,13 +1,6 @@
 CREATE DATABASE bibliotechDB;
 USE bibliotechDB;
 
-CREATE TABLE `Multa` (
-    `id_multa` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `valor_dia_atraso` DECIMAL(10,2) NOT NULL,
-    `valor_final` DECIMAL(10,2) NOT NULL,
-    `dias_atraso` INT NOT NULL
-);
-
 CREATE TABLE `Usuarios` (
     `id_usuario` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `nome` VARCHAR(255) NOT NULL,
@@ -38,13 +31,10 @@ CREATE TABLE `Emprestimos` (
     `data_prazo` VARCHAR(10) NOT NULL,
     `data_devolucao` VARCHAR(10) NOT NULL,
     `horario` VARCHAR(8) NOT NULL,
-    `id_multa` INT UNSIGNED,
     `id_usuario` INT UNSIGNED NOT NULL,
     `id_livro` INT UNSIGNED NOT NULL,
-    INDEX (`id_multa`),
     INDEX (`id_usuario`),
     INDEX (`id_livro`),
-    FOREIGN KEY (`id_multa`) REFERENCES `Multa`(`id_multa`),
     FOREIGN KEY (`id_usuario`) REFERENCES `Usuarios`(`id_usuario`),
     FOREIGN KEY (`id_livro`) REFERENCES `Livros`(`id_livro`)
 );
